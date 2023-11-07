92c36c85c56df6c71ed4d7fc7c51ebae8bf1b8b0
123nush <anushkadal1611@gmail.com>
2023-11-07 11:38:10 +0530

    python and java files

---
+++ file2.java
@@ -0,0 +1,25 @@
+public class file2 {
+    private String name;
+    private int age;
+
+    public file2(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
